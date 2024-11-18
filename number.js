e294e0a8fb31993953ccc1e0ba52cdd24cfd6388
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-11-18 16:35:31 +0500

    Add files via upload

---
+++ number.js
@@ -0,0 +1,66 @@
+
+// esi valur jo number na ho string ya kch b ho use number me convert krne k liye NUMBER METHOD () ka use krenge
+
+
+
+
+// let integerString = '123';
+// console.log(Number(integerString) + 2);
+// output....... 125
+// .
+// console.log(Number("123.456"));
+// output ............123.456
+
+// console.log(Number("123 456778"));
+// output ............NaN
+
+// console.log(Number(false));
+// output ............0
+
+// console.log(Number(true));
+// output ............1
+
+// console.log(Number("123 456778"));
+// output ............NaN
+
+
+// console.log(Number());
+// output ............0
+
+// console.log(Number("123ab"));
+// output ............NaN
+
+
+
+
+
+
+
+
+// DATE 
+// console.log(new Date());
+// output ..........Fri Nov 15 2024 15:38:37 GMT+0500 (Pakistan Standard Time)
+
+
+
+// PARSEINT
+// console.log(parseInt("123.456"));
+// output....... 123
+
+
+// PARSEFLOAT 
+// console.log(parseFloat("123.456"));
+// output ............123.456
+
+
+
+//NUMBER IN ARRAY
+
+// console.log(Number([9]));
+// output ............9
+
+// console.log(Number([9.8]));
+// output ............9.8
+
+// console.log(Number([9,8]));
+// output ............NaN
